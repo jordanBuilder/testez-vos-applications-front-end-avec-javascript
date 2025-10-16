d07407de99b1920083d03d1958e6e3e33ada3d8d
jordan Tomegah <tomegahjordan81@gmail.com>
2025-10-16 14:18:20 +0000

    testing test coverage

--- js/samples/unit/unit1.js
+++ js/samples/unit/unit1.js
@@ -6,6 +6,11 @@ export const sayHello = name => {
     if (!name) {
         return "Hello, World"
     }
-
+    if (name === "Alexandra") {
+        return "Hello, Alexandra !"
+    }
+    if(name === "Jordan"){
+        return "Hello, Jordan !"
+    }
     return `Hello, ${name}`
 }
